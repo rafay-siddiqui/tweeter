d533490ba6964d6ed077e92374a6952b8de28109
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-08-15 09:25:37 +0000

    Added character counter with colour change functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  console.log("Good to go chief");
+  console.log("waiting...");
+
+  //Used multiple event triggers to account for mouse drag pasting text into textarea
+  $('#tweet-text').on('keyup input', function(event) {
+    const tweetLength = $(this).val().length;
+    $(this).parent().find('output').html(140 - tweetLength);
+    //Couldn't figure out how to do this using only CSS
+    if (tweetLength > 140) {
+      $(this).parent().find('output').css('color', '#FF0000');
+    } else {
+      $(this).parent().find('output').css('color', 'inherit');
+    }
+  });
+
 });
