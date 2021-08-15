4f99f5b521566269a2dde521b8bf9d899abfbcea
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-08-15 13:10:55 +0000

    Change char counter JQUERY logic to toggle counter colour using CSS through a class toggle

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   console.log("waiting...");
-
+  
   //Used multiple event triggers to account for mouse drag pasting text into textarea
   $('#tweet-text').on('keyup input', function(event) {
     const tweetLength = $(this).val().length;
     $(this).parent().find('output').html(140 - tweetLength);
-    //Couldn't figure out how to do this using only CSS
+
+    //Toggles 'charLimit' class in counter output to toggle colour change using CSS
     if (tweetLength > 140) {
-      $(this).parent().find('output').css('color', '#FF0000');
+      $(this).parent().find('output').addClass('charLimit');
     } else {
-      $(this).parent().find('output').css('color', 'inherit');
+      $(this).parent().find('output').removeClass('charLimit');
     }
   });
 
