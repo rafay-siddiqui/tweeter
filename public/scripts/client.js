1918500ae10218c3c018771fa91a48540f24f1eb
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-08-16 06:03:20 +0000

    Implement renderTweets which loops through and renders all tweets in tweet objects array

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,32 @@
 
 $(document).ready(() => {
 
-const createTweetElement = (tweetObj) => {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const createTweetElement = (tweetObj) => {
   const timePassed = timeago.format(tweetObj.created_at);
   const $tweet = $(`
   <article class="tweet">
@@ -34,19 +59,12 @@ const createTweetElement = (tweetObj) => {
   return $tweet;
 };
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+const renderTweets = (tweetsArray) => {
+  for (let obj of tweetsArray) {
+    $('#tweets-container').append(createTweetElement(obj));
+  }
+}
 
-const $tweet = createTweetElement(tweetData);
+renderTweets(data);
 
-  $('#tweets-container').append($tweet);
 })
